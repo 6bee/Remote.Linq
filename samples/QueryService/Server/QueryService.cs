fb06db84305411e5259fb5c5dbd49847fce02104
ChristofSenn <christof.senn@gmail.com>
2014-09-01 03:32:24 +0200

    added sample application to show dynamic query service

---
+++ samples/QueryService/Server/QueryService.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Christof Senn. All rights reserved. See license.txt in the project root for license information.
+
+using Common.Model;
+using Common.ServiceContracts;
+using Remote.Linq.Dynamic;
+using Remote.Linq.Expressions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class QueryService : IQueryService
+    {
+        private static readonly Func<Type, IQueryable> _queryableResourceProvider = type =>
+        {
+            var dataStore = InMemoryDataStore.Instance;
+
+            if (type == typeof(ProductCategory)) return dataStore.ProductCategories.AsQueryable();
+            if (type == typeof(Product)) return dataStore.Products.AsQueryable();
+            if (type == typeof(OrderItem)) return dataStore.OrderItems.AsQueryable();
+
+            throw new Exception(string.Format("No queryable resource available for type {0}", type));
+        };
+
+        public async Task<IEnumerable<DynamicObject>> ExecuteQueryAsync(Expression queryExpression)
+        {
+            return await Task.Run(() => queryExpression.Execute(_queryableResourceProvider)).ConfigureAwait(false);
+        }
+    }
+}
