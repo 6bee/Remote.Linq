52007da30712f1fff15d3ec5fb381d61daec6137
ChristofSenn <christof.senn@gmail.com>
2015-05-01 23:06:49 +0200

    include missing test file

---
+++ src/Remote.Linq/Remote.Linq.Tests/Serialization/Expressions/When_using_complex_expression_with_local_lambda.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) Christof Senn. All rights reserved. See license.txt in the project root for license information.
+
+namespace Remote.Linq.Tests.Serialization.Expressions
+{
+    using Remote.Linq.Expressions;
+    using System;
+    using Xunit;
+
+    public class When_using_complex_expression_with_local_lambda
+    {
+        private LambdaExpression _remoteExpression;
+
+        private LambdaExpression _serializedRemoteExpression;
+
+        public When_using_complex_expression_with_local_lambda()
+        {
+            Func<object, string> sufix = (x) => x + "ending";
+
+            System.Linq.Expressions.Expression<Func<bool, bool>> expression = x => sufix("test").Length > 10;
+
+            _remoteExpression = expression.ToRemoteLinqExpression();
+
+            // HINT: since this test is used in multiple assemblies as linked file, 
+            //       use serialize extension method have context specific serialization applied
+            _serializedRemoteExpression = _remoteExpression.SerializeExpression();
+        }
+
+        [Fact]
+        public void Remote_expression_should_be_equal()
+        {
+            var str1 = _remoteExpression.ToString();
+            var str2 = _serializedRemoteExpression.ToString();
+
+            _remoteExpression.EqualsRemoteExpression(_serializedRemoteExpression);
+        }
+
+        [Fact]
+        public void System_expresison_should_be_equal()
+        {
+            var exp1 = _remoteExpression.ToLinqExpression<bool, bool>();
+            var exp2 = _serializedRemoteExpression.ToLinqExpression<bool, bool>();
+
+            exp1.EqualsExpression(exp2);
+        }
+    }
+}
