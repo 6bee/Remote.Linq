ecd8469df32b906da4e91bdd732c61dc8b2672ab
ChristofSenn <christof.senn@gmail.com>
2015-09-01 23:14:40 +0200

    added sample demo application to demonstrate inheritance scenario and querying abstract data type

---
+++ samples/RemoteQueryable/7_Inheritance/Client/Demo.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (c) Christof Senn. All rights reserved. See license.txt in the project root for license information.
+
+namespace Client
+{
+    using Common.Model;
+    using Remote.Linq;
+    using System;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class Demo
+    {
+        private readonly string _url;
+
+        public Demo(string url)
+        {
+            _url = url;
+        }
+
+        public void Run()
+        {
+            var repo = new RemoteRepository(_url);
+
+            Console.WriteLine("\nGET ALL ENTITIES:");
+            foreach (var i in repo.Entities)
+            {
+                Console.WriteLine("  {0}: {1}", i.Id, i);
+            }
+
+
+            Console.WriteLine("\nGET ALL ENTITIES OF TYPE PRODUCT:");
+            foreach (var i in repo.Entities.OfType<Product>())
+            {
+                Console.WriteLine("  {0}: {1}", i.Id, i.Name);
+            }
+
+
+            Console.WriteLine("\nSELECT IDs:");
+            var entityIdsQuery =
+                from e in repo.Entities
+                orderby e.Id ascending
+                select new { EntityId = e.Id };
+            foreach (var item in entityIdsQuery)
+            {
+                Console.WriteLine("  {0}", item.EntityId);
+            }
+
+
+            Console.WriteLine("\nCOUNT:");
+            var entitiesQuery =
+                from p in repo.Entities
+                select p;
+            Console.WriteLine("  Count = {0}", entitiesQuery.Count());
+        }
+    }
+}
---
+++ samples/RemoteQueryable/7_Inheritance/Client/RemoteRepository.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (c) Christof Senn. All rights reserved. See license.txt in the project root for license information.
+
+namespace Client
+{
+    using Aqua.Dynamic;
+    using Common.Model;
+    using Common.ServiceContracts;
+    using Remote.Linq;
+    using Remote.Linq.Expressions;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.ServiceModel;
+
+    public class RemoteRepository : IDisposable
+    {
+        private readonly ChannelFactory<IQueryService> _channelFactory;
+
+        private readonly Func<Expression, IEnumerable<DynamicObject>> _dataProvider;
+
+        public RemoteRepository(string uri)
+        {
+            var binding = new NetNamedPipeBinding()
+            {
+                CloseTimeout = TimeSpan.FromMinutes(10),
+                ReceiveTimeout = TimeSpan.FromMinutes(10),
+                SendTimeout = TimeSpan.FromMinutes(10),
+                MaxReceivedMessageSize = 640000L
+            };
+
+            _channelFactory = new ChannelFactory<IQueryService>(binding, uri);
+
+            _dataProvider = expression =>
+                {
+                    IQueryService channel = null;
+                    try
+                    {
+                        channel = _channelFactory.CreateChannel();
+
+                        var result = channel.ExecuteQuery(expression);
+                        return result;
+                    }
+                    finally
+                    {
+                        var communicationObject = channel as ICommunicationObject;
+                        if (communicationObject != null)
+                        {
+                            if (communicationObject.State == CommunicationState.Faulted)
+                            {
+                                communicationObject.Abort();
+                            }
+                            else
+                            {
+                                communicationObject.Close();
+                            }
+                        }
+                    }
+                };
+        }
+
+        public IQueryable<Entity> Entities { get { return RemoteQueryable.Create<Entity>(_dataProvider); } }
+
+        public void Dispose()
+        {
+            ((IDisposable)_channelFactory).Dispose();
+        }
+    }
+}
