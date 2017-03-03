3b2e881311d26933528f3da271db4919b82fdb22
Christof Senn <christof.senn@gmail.com>
2017-03-03 04:04:29 +0100

    fixed typo

--- src/Remote.Linq.Newtonsoft.Json/JsonConverters/ConstantExpressionJsonConverter.cs
+++ src/Remote.Linq.Newtonsoft.Json/JsonConverters/ConstantExpressionJsonConverter.cs
@@ -15,7 +15,7 @@ namespace Remote.Linq.JsonConverters
         
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
-            // this mehods doesn't get invoked for deserialization for some reason...?
+            // this method doesn't get invoked for deserialization for some reason...!?
             throw new NotImplementedException();
 
             //if (reader.TokenType == JsonToken.Null)
