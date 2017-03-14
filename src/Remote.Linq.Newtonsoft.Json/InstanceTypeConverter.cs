c1af2f4c2e15125ca7843c4b331cd4c64925461b
Christof Senn <christof.senn@gmail.com>
2017-03-15 00:57:08 +0100

    re-use primitive value converter from aqua-core-newtonsoft-json package

--- src/Remote.Linq.Newtonsoft.Json/InstanceTypeConverter.cs
+++ src/Remote.Linq.Newtonsoft.Json/InstanceTypeConverter.cs
@@ -2,9 +2,9 @@
 
 namespace Remote.Linq
 {
+    using Aqua;
     using Remote.Linq.DynamicQuery;
     using Remote.Linq.Expressions;
-    using System;
     using System.Linq;
     using System.Runtime.Serialization;
 
@@ -15,7 +15,7 @@ namespace Remote.Linq
             var constantExpression = o as ConstantExpression;
             if (!ReferenceEquals(null, constantExpression?.Value))
             {
-                var converter = GetConverter(constantExpression.Type);
+                var converter = PrimitiveValueInspector.GetConverter(constantExpression.Type);
                 if (!ReferenceEquals(null, converter))
                 {
                     constantExpression.Value = converter(constantExpression.Value);
@@ -28,7 +28,7 @@ namespace Remote.Linq
             var variableQueryArgument = o as VariableQueryArgument;
             if (!ReferenceEquals(null, variableQueryArgument?.Value))
             {
-                var converter = GetConverter(variableQueryArgument.Type);
+                var converter = PrimitiveValueInspector.GetConverter(variableQueryArgument.Type);
                 if (!ReferenceEquals(null, converter))
                 {
                     variableQueryArgument.Value = converter(variableQueryArgument.Value);
@@ -41,38 +41,12 @@ namespace Remote.Linq
             var variableQueryArgumentList = o as VariableQueryArgumentList;
             if (!ReferenceEquals(null, variableQueryArgumentList?.Values))
             {
-                var converter = GetConverter(variableQueryArgumentList.ElementType);
+                var converter = PrimitiveValueInspector.GetConverter(variableQueryArgumentList.ElementType);
                 if (!ReferenceEquals(null, converter))
                 {
                     variableQueryArgumentList.Values = variableQueryArgumentList.Values.Select(converter).ToList();
                 }
             }
-        }
-
-        private static Func<object, object> GetConverter(Aqua.TypeSystem.TypeInfo typeInfo)
-        {
-            if (ReferenceEquals(null, typeInfo))
-            {
-                return null;
-            }
-
-            if (string.Equals(typeInfo.FullName, typeof(int).FullName))
-            {
-                return x => Convert.ToInt32(x);
-            }
-
-            if (string.Equals(typeInfo.FullName, typeof(char).FullName))
-            {
-                return x => Convert.ToChar(x);
-            }
-
-            if (string.Equals(typeInfo.FullName, typeof(Nullable<>).FullName) && typeInfo.GenericArguments?.Count == 1)
-            {
-                var converter = GetConverter(typeInfo.GenericArguments.Single());
-                return x => ReferenceEquals(null, x) ? null : converter(x);
-            }
-
-            return null;
         }
     }
 }
