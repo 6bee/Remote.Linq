aed1ae8bb373163378a4d8aed7f82059774ef4be
Christof Senn <christof.senn@gmail.com>
2017-06-24 13:06:50 +0200

    5.5.0-alpha-003

--- pack.bat
+++ pack.bat
@@ -1,9 +1,11 @@
 @echo off
+set configuration=Debug
+set version-suffix="003"
 clean ^
   && dotnet restore ^
-  && dotnet build src\Remote.Linq ^
-  && dotnet build src\Remote.Linq.Newtonsoft.Json ^
-  && dotnet build test\Remote.Linq.Tests ^
-  && dotnet test test\Remote.Linq.Tests\Remote.Linq.Tests.csproj ^
-  && dotnet pack src\Remote.Linq --output "..\..\artifacts" --configuration Debug --include-symbols --version-suffix 002 ^
-  && dotnet pack src\Remote.Linq.Newtonsoft.Json --output "..\..\artifacts" --configuration Debug --include-symbols --version-suffix 002
+  && dotnet build src\Remote.Linq --configuration %configuration% ^
+  && dotnet build src\Remote.Linq.Newtonsoft.Json --configuration %configuration% ^
+  && dotnet build test\Remote.Linq.Tests --configuration %configuration% ^
+  && dotnet test test\Remote.Linq.Tests\Remote.Linq.Tests.csproj --configuration %configuration% ^
+  && dotnet pack src\Remote.Linq --output "..\..\artifacts" --configuration %configuration% --include-symbols --version-suffix %version-suffix% ^
+  && dotnet pack src\Remote.Linq.Newtonsoft.Json --output "..\..\artifacts" --configuration %configuration% --include-symbols --version-suffix %version-suffix%
