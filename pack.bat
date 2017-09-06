433136b9b18ef06af591dd3684d017dc8d22e1ba
Christof Senn <christof.senn@gmail.com>
2017-09-06 06:52:49 +0200

    put version-suffix in quotes if empty

--- pack.bat
+++ pack.bat
@@ -8,4 +8,4 @@ clean ^
   && dotnet build test\Remote.Linq.Tests --configuration %configuration% ^
   && dotnet test test\Remote.Linq.Tests\Remote.Linq.Tests.csproj --configuration %configuration% ^
   && dotnet pack src\Remote.Linq --output "..\..\artifacts" --configuration %configuration% --include-symbols --version-suffix %version-suffix% ^
-  && dotnet pack src\Remote.Linq.Newtonsoft.Json --output "..\..\artifacts" --configuration %configuration% --include-symbols --version-suffix %version-suffix%
+  && dotnet pack src\Remote.Linq.Newtonsoft.Json --output "..\..\artifacts" --configuration %configuration% --include-symbols --version-suffix "%version-suffix%"
