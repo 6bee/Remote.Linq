2783fef549d37ef2f9bd4daa283ff1aa42247303
Christof Senn <christof.senn@gmail.com>
2017-09-21 17:29:30 +0200

    5.6.0

--- pack.bat
+++ pack.bat
@@ -1,11 +1,11 @@
 @echo off
-set configuration=Debug
-set version-suffix="003"
+set configuration=Release
+set version-suffix=""
 clean ^
   && dotnet restore ^
   && dotnet build src\Remote.Linq --configuration %configuration% ^
   && dotnet build src\Remote.Linq.Newtonsoft.Json --configuration %configuration% ^
   && dotnet build test\Remote.Linq.Tests --configuration %configuration% ^
   && dotnet test test\Remote.Linq.Tests\Remote.Linq.Tests.csproj --configuration %configuration% ^
-  && dotnet pack src\Remote.Linq --output "..\..\artifacts" --configuration %configuration% --include-symbols --version-suffix %version-suffix% ^
+  && dotnet pack src\Remote.Linq --output "..\..\artifacts" --configuration %configuration% --include-symbols --version-suffix "%version-suffix%" ^
   && dotnet pack src\Remote.Linq.Newtonsoft.Json --output "..\..\artifacts" --configuration %configuration% --include-symbols --version-suffix "%version-suffix%"
