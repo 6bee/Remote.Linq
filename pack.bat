9cc627db1ce8b7ad3c69884726159e74faef1547
Christof Senn <christof.senn@gmail.com>
2017-09-21 07:09:13 +0200

    5.6.0-alpha-003

--- pack.bat
+++ pack.bat
@@ -1,6 +1,6 @@
 @echo off
 set configuration=Debug
-set version-suffix="002"
+set version-suffix="003"
 clean ^
   && dotnet restore ^
   && dotnet build src\Remote.Linq --configuration %configuration% ^
