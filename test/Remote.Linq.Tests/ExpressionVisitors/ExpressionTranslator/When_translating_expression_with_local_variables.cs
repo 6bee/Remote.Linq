f449a0b696ec595ca9c0dd05727da95ebf810b10
Senn, Christof <Christof.Senn@bis.org>
2018-01-18 09:05:20 +0100

    moved filter criteria for skipping local evaluation of expressions from "evaluation" to "nomination" in order to allow for further tree iteration #36

---
+++ test/Remote.Linq.Tests/ExpressionVisitors/ExpressionTranslator/When_translating_expression_with_local_variables.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Christof Senn. All rights reserved. See license.txt in the project root for license information.
+
+namespace Remote.Linq.Tests.ExpressionVisitors.ExpressionTranslator
+{
+    using Remote.Linq;
+    using System;
+    using System.Linq.Expressions;
+    using Xunit;
+
+    public class When_translating_expression_with_local_variables
+    {
+        class A
+        {
+            public short Property { get; set; }
+        }
+
+        [Fact]
+        public void Should_resolve_short_argument_to_variable()
+        {
+            short argument = 1;
+
+            Expression<Func<A, bool>> expr = x => x.Property == argument;
+
+            var remoteExpr = expr.ToRemoteLinqExpression();
+
+            var stringRepresentation = remoteExpr.ToString();
+            stringRepresentation.ShouldMatch("x => (Convert(x.Property) Equal Convert(New Remote.Linq.DynamicQuery.VariableQueryArgument`1[System.Int16](1).Value))");
+        }
+
+        [Fact]
+        public void Should_resolve_int_argument_to_variable()
+        {
+            int parameter = 1;
+
+            Expression<Func<A, bool>> expr = x => x.Property == parameter;
+            
+            var remoteExpr = expr.ToRemoteLinqExpression();
+
+            var stringRepresentation = remoteExpr.ToString();
+            stringRepresentation.ShouldMatch("x => (Convert(x.Property) Equal New Remote.Linq.DynamicQuery.VariableQueryArgument`1[System.Int32](1).Value)");
+        }
+    }
+}
