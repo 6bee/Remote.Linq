3fb49e499bf769e65e4d20da146168a107542729
Christof Senn <christof.senn@gmail.com>
2019-06-30 06:32:37 +0200

    considering array instead of single instance to indicate `InvalidOperationException` of `Single()`, `First()`, and `Last()` operations

--- src/Remote.Linq/DynamicQuery/ObjectResultCaster.cs
+++ src/Remote.Linq/DynamicQuery/ObjectResultCaster.cs
@@ -2,11 +2,44 @@
 
 namespace Remote.Linq.DynamicQuery
 {
+    using Aqua.TypeSystem;
+    using System;
     using System.Linq.Expressions;
+    using System.Reflection;
 
     internal sealed class ObjectResultCaster : IQueryResultMapper<object>
     {
         public TResult MapResult<TResult>(object source, Expression expression)
-            => (TResult)source;
+        {
+            var sourceType = source?.GetType();
+            if (sourceType?.IsArray ?? false)
+            {
+                var elementType = TypeHelper.GetElementType(sourceType);
+                if (typeof(TResult).IsAssignableFrom(elementType))
+                {
+                    try
+                    {
+                        if ((expression as MethodCallExpression)?.Arguments.Count == 2)
+                        {
+                            var predicate = GetTruePredicate(elementType);
+                            source = MethodInfos.Enumerable.SingleWithFilter.MakeGenericMethod(elementType).Invoke(null, new object[] { source, predicate });
+                        }
+                        else
+                        {
+                            source = MethodInfos.Enumerable.Single.MakeGenericMethod(elementType).Invoke(null, new object[] { source });
+                        }
+                    }
+                    catch (TargetInvocationException ex)
+                    {
+                        throw ex.InnerException;
+                    }
+                }
+            }
+
+            return (TResult)source;
+        }
+
+        private static object GetTruePredicate(Type t)
+            => Expression.Lambda(Expression.Constant(true), Expression.Parameter(t)).Compile();
     }
 }
