506675815acc2cbd1019de5e6a4360f602477248
Christof Senn <christof.senn@gmail.com>
2019-08-22 23:39:55 +0200

    6.1.0-beta-001

--- pack.bat
+++ pack.bat
@@ -1,6 +1,6 @@
 @echo off
-set configuration=Release
-set version-suffix=""
+set configuration=Debug
+set version-suffix="beta-001"
 clean ^
   && dotnet restore ^
   && dotnet build src\Remote.Linq                           --configuration %configuration% ^
