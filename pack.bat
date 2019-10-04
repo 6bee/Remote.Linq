ba727a9ac980dcffb3c3f894f886ca33f30ce0a4
Christof Senn <christof.senn@gmail.com>
2019-10-04 22:04:23 +0200

    restructured project config (*.props and style*.ruleset)

--- pack.bat
+++ pack.bat
@@ -1,16 +1,15 @@
 @echo off
 set configuration=Debug
-set version-suffix="alpha-001"
 clean ^
   && dotnet restore ^
-  && dotnet build src\Remote.Linq                           --configuration %configuration% --version-suffix "%version-suffix%" ^
-  && dotnet build src\Remote.Linq.EntityFramework           --configuration %configuration% --version-suffix "%version-suffix%" ^
-  && dotnet build src\Remote.Linq.EntityFrameworkCore       --configuration %configuration% --version-suffix "%version-suffix%" ^
-  && dotnet build src\Remote.Linq.Newtonsoft.Json           --configuration %configuration% --version-suffix "%version-suffix%" ^
+  && dotnet build src\Remote.Linq                           --configuration %configuration% ^
+  && dotnet build src\Remote.Linq.EntityFramework           --configuration %configuration% ^
+  && dotnet build src\Remote.Linq.EntityFrameworkCore       --configuration %configuration% ^
+  && dotnet build src\Remote.Linq.Newtonsoft.Json           --configuration %configuration% ^
   && dotnet test test\Remote.Linq.Tests                     --configuration %configuration% ^
   && dotnet test test\Remote.Linq.EntityFrameworkCore.Tests --configuration %configuration% ^
   && dotnet test test\Remote.Linq.EntityFramework.Tests     --configuration %configuration% ^
-  && dotnet pack src\Remote.Linq                            --configuration %configuration% --version-suffix "%version-suffix%" --include-symbols --include-source --output "artifacts" ^
-  && dotnet pack src\Remote.Linq.Newtonsoft.Json            --configuration %configuration% --version-suffix "%version-suffix%" --include-symbols --include-source --output "artifacts" ^
-  && dotnet pack src\Remote.Linq.EntityFramework            --configuration %configuration% --version-suffix "%version-suffix%" --include-symbols --include-source --output "artifacts" ^
-  && dotnet pack src\Remote.Linq.EntityFrameworkCore        --configuration %configuration% --version-suffix "%version-suffix%" --include-symbols --include-source --output "artifacts"
+  && dotnet pack src\Remote.Linq                            --configuration %configuration% --include-symbols --include-source --output "artifacts" ^
+  && dotnet pack src\Remote.Linq.Newtonsoft.Json            --configuration %configuration% --include-symbols --include-source --output "artifacts" ^
+  && dotnet pack src\Remote.Linq.EntityFramework            --configuration %configuration% --include-symbols --include-source --output "artifacts" ^
+  && dotnet pack src\Remote.Linq.EntityFrameworkCore        --configuration %configuration% --include-symbols --include-source --output "artifacts"
