d2cfc61c3fd552488ca1c0266ad9926827193e76
Christof Senn <christof.senn@gmail.com>
2020-05-26 13:43:15 +0200

    resolved compile issue

--- src/Remote.Linq.EntityFramework/ExpressionExecution/DefaultEntityFrameworkExpressionExecutor.cs
+++ src/Remote.Linq.EntityFramework/ExpressionExecution/DefaultEntityFrameworkExpressionExecutor.cs
@@ -19,7 +19,7 @@ namespace Remote.Linq.EntityFramework.ExpressionExecution
 
         [SecuritySafeCritical]
         public DefaultEntityFrameworkExpressionExecutor(DbContext dbContext, ITypeResolver? typeResolver = null, IDynamicObjectMapper? mapper = null, Func<Type, bool>? setTypeInformation = null, Func<System.Linq.Expressions.Expression, bool>? canBeEvaluatedLocally = null)
-            : this(GetQueryableSetProvider(dbContext), typeResolver, mapper, setTypeInformation, canBeEvaluatedLocally)
+            : this(dbContext.GetQueryableSetProvider(), typeResolver, mapper, setTypeInformation, canBeEvaluatedLocally)
         {
         }
 
