3fafe492eeb31f4737d50976231a181dcb3c8a24
Christof Senn <christof.senn@gmail.com>
2020-06-02 21:49:30 +0200

    added sample application for async streams initial version for #69 complete

---
+++ samples/03_RemoteQueryable_AsyncStream/Server/QueryService.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Christof Senn. All rights reserved. See license.txt in the project root for license information.
+
+namespace Server
+{
+    using Aqua.Dynamic;
+    using Remote.Linq.Expressions;
+    using Remote.Linq.TestSupport;
+    using System.Collections.Generic;
+    using System.Threading;
+
+    public class QueryService
+    {
+        private InMemoryDataStore DataStore => InMemoryDataStore.Instance;
+
+        public IAsyncEnumerable<DynamicObject> ExecuteAsyncStreamQuery(Expression queryExpression, CancellationToken cancellation)
+        {
+            // DEMO: for demo purpose we fetch query result into memory
+            // and use test-support extension method to return an asyn stream
+            var result = queryExpression.Execute(DataStore.QueryableByTypeProvider);
+            return result.AsAsyncRemoteStreamQueryable().ExecuteAsyncRemoteStream(cancellation);
+        }
+    }
+}
