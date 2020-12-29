609b164e89374344df70fa750fd7ce4b94e08819
Christof Senn <christof.senn@gmail.com>
2020-12-29 16:27:53 +0100

    include coverage results

--- clean.bat
+++ clean.bat
@@ -1,3 +1,3 @@
 @echo off
-for /d /r . %%d in (artifacts,packages,TestResults,bin,obj,.vs) do @if exist "%%d" rd /s /q "%%d"
-for    /r . %%f in (*.bak,*.user,*.suo)                         do @if exist "%%f" del "%%f"
+for /d /r . %%d in (artifacts,packages,TestResults,bin,obj,.vs)      do @if exist "%%d" rd /s /q "%%d"
+for    /r . %%f in (*.bak,*.user,*.suo,coverage*.json,coverage*.xml) do @if exist "%%f" del "%%f"
