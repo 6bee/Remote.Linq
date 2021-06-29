017a67405a81beddca6d053f938da1cc04c71978
Christof Senn <christof.senn@gmail.com>
2021-06-29 15:30:07 +0200

    removed unused InternalsVisibleToAttribute

--- src/Remote.Linq.protobuf-net/Properties/AssemblyInfo.cs
+++ src/Remote.Linq.protobuf-net/Properties/AssemblyInfo.cs
@@ -1,15 +1,7 @@
 ﻿// Copyright (c) Christof Senn. All rights reserved. See license.txt in the project root for license information.
 
 using System.Reflection;
-using System.Runtime.CompilerServices;
 
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-[assembly: InternalsVisibleTo("Remote.Linq.protobuf-net.Test, PublicKey=" +
-"0024000004800000940000000602000000240000525341310004000001000100953fd5a46105ee" +
-"9924e16998b8d7cad276601925390edd925c567e11a6d22ad43f35db27a94b2b3288886ddbb560" +
-"53f3310bb7707ea653b2aed436936aea7aa6ddea4598a4eb9516aafcc3e26ff3e370a57634380a" +
-"a7af3a678e96c00aacca6d460cbb7c9d2bdbed375a40cf89430a404314bec9b7f4498b24017548" +
-"f3dd92ed")]
