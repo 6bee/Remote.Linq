636bd3cded1f0628343008647b720b788c65c2aa
Christof Senn <christof.senn@gmail.com>
2023-12-13 23:18:44 +0100

    build packages (dependencies) before building and running samples

--- run_all_samples.bat
+++ run_all_samples.bat
@@ -1,4 +1,5 @@
 @echo off
+dotnet build "%~dp0Remote.Linq.sln" -p GeneratePackageOnBuild=true
 for /r "%~dp0samples\" %%f in (DemoStartUp.csproj) do @if exist "%%f" dotnet run --project "%%f" --non-interactive
 echo.
 echo ALL DONE.
