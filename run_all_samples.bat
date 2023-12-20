0b60b3d470fbf9d3fd2b865731610fd5d5e9634f
Christof Senn <christof.senn@gmail.com>
2023-12-20 23:54:54 +0100

    suppress build output

--- run_all_samples.bat
+++ run_all_samples.bat
@@ -1,5 +1,7 @@
 @echo off
-dotnet build "%~dp0Remote.Linq.sln" -p GeneratePackageOnBuild=true
+echo BUILD FRAMEWORK
+dotnet build "%~dp0Remote.Linq.sln" -p GeneratePackageOnBuild=true > nul
+echo RUN SAMPLES
 for /r "%~dp0samples\" %%f in (DemoStartUp.csproj) do @if exist "%%f" dotnet run --project "%%f" --non-interactive
 echo.
 echo ALL DONE.
