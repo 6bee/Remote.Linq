8d52552b2428d6324b9adedb91e0f786c1139d45
Christof Senn <christof.senn@gmail.com>
2023-12-21 11:17:16 +0100

    use ProjectReference in place of PackageReference to reference remote linq libraries

--- run_all_samples.bat
+++ run_all_samples.bat
@@ -1,7 +1,4 @@
 @echo off
-echo BUILD FRAMEWORK
-dotnet build "%~dp0Remote.Linq.sln" -p GeneratePackageOnBuild=true > nul
-echo RUN SAMPLES
 for /r "%~dp0samples\" %%f in (DemoStartUp.csproj) do @if exist "%%f" dotnet run --project "%%f" --non-interactive
 echo.
 echo ALL DONE.
