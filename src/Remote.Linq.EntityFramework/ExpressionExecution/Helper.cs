c24fb5105440fe10c4393f63a2cdf93a61267882
Christof Senn <christof.senn@gmail.com>
2024-06-11 09:29:45 +0200

    make GetQueryableSetProvider extension method public

--- src/Remote.Linq.EntityFramework/ExpressionExecution/Helper.cs
+++ src/Remote.Linq.EntityFramework/ExpressionExecution/Helper.cs
@@ -3,12 +3,10 @@
 namespace Remote.Linq.EntityFramework.ExpressionExecution;
 
 using Aqua.TypeExtensions;
-using System;
 using System.Data.Entity;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Reflection;
-using System.Security;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -25,58 +23,17 @@ internal static class Helper
         }
     }
 
-    /// <summary>
-    /// Type definition used in generic type filters.
-    /// </summary>
-    [SuppressMessage("Major Bug", "S3453:Classes should not have only \"private\" constructors", Justification = "For reflection only")]
-    private sealed class TEntity
-    {
-        private TEntity()
-        {
-        }
-    }
-
-    private static readonly MethodInfo QueryableToListAsyncMethod = typeof(QueryableExtensions).GetMethodEx(
+    private static readonly MethodInfo _queryableToListAsyncMethod = typeof(QueryableExtensions).GetMethodEx(
         nameof(QueryableExtensions.ToListAsync),
         [typeof(TSource)],
         typeof(IQueryable<TSource>),
         typeof(CancellationToken));
 
-    private static readonly MethodInfo DbContextSetMethod = typeof(DbContext).GetMethodEx(
-        nameof(DbContext.Set),
-        genericArguments: [typeof(TEntity)]);
-
     internal static Task ToListAsync(IQueryable source, CancellationToken cancellation)
     {
         source.AssertNotNull();
-        var method = QueryableToListAsyncMethod.MakeGenericMethod(source.ElementType);
+        var method = _queryableToListAsyncMethod.MakeGenericMethod(source.ElementType);
         var task = method.Invoke(null, [source, cancellation]);
         return (Task)task!;
-    }
-
-    /// <summary>
-    /// Returns the generic <see cref="DbSet{T}"/> for the type specified.
-    /// </summary>
-    /// <returns>Returns an instance of type <see cref="DbSet{T}"/>.</returns>
-    [SecuritySafeCritical]
-    internal static Func<Type, IQueryable> GetQueryableSetProvider(this DbContext dbContext)
-        => new QueryableSetProvider(dbContext).GetQueryableSet;
-
-    [SecuritySafeCritical]
-    private sealed class QueryableSetProvider
-    {
-        private readonly DbContext _dbContext;
-
-        [SecuritySafeCritical]
-        public QueryableSetProvider(DbContext dbContext)
-            => _dbContext = dbContext.CheckNotNull();
-
-        [SecuritySafeCritical]
-        public IQueryable GetQueryableSet(Type type)
-        {
-            var method = DbContextSetMethod.MakeGenericMethod(type);
-            var set = method.Invoke(_dbContext, null);
-            return (IQueryable)set!;
-        }
     }
 }
