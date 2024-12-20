e6fa1d59c3d1323e682555b5c52941a6cc5e3007
Christof Senn <christof.senn@gmail.com>
2024-12-20 22:54:54 +0100

    make ProtoBufTypeModel visible

--- src/Remote.Linq.protobuf-net/ProtoBufTypeModel.cs
+++ src/Remote.Linq.protobuf-net/ProtoBufTypeModel.cs
@@ -6,9 +6,7 @@ using Aqua.ProtoBuf;
 using global::ProtoBuf.Meta;
 using Remote.Linq.ProtoBuf.DynamicQuery;
 using Remote.Linq.ProtoBuf.Expressions;
-using System.ComponentModel;
 
-[EditorBrowsable(EditorBrowsableState.Never)]
 public static class ProtoBufTypeModel
 {
     public static AquaTypeModel ConfigureRemoteLinq(string? name = null, bool configureDefaultSystemTypes = true)
