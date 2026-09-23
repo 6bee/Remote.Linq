2f88f6787d1dbb53bb4274892bae7afacaee47d7
Christof Senn <christof.senn@option.ch>
2026-09-23 16:03:19 +0200

    re-declare Expression property to hide both IRemoteLinqQueryable.Expression and IQueryable.Expression, to prevent ambiguity

--- src/Remote.Linq/IRemoteQueryable.cs
+++ src/Remote.Linq/IRemoteQueryable.cs
@@ -2,11 +2,18 @@
 
 namespace Remote.Linq;
 
+using System.Linq.Expressions;
+
 /// <summary>
 /// Provides functionality to compose queries for remote execution.
 /// </summary>
 public interface IRemoteQueryable : IRemoteLinqQueryable, IQueryable
 {
+    /// <summary>
+    /// Gets the expression tree that is associated with the instance of <see cref="IRemoteQueryable"/>.
+    /// </summary>
+    new Expression Expression { get; } // hides both IRemoteLinqQueryable.Expression and IQueryable.Expression to resolve ambiguity
+
     /// <summary>
     /// Gets the query provider that is associated with this data source.
     /// </summary>
